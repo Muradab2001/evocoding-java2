ce0698b9b8f02ae9201b1434e5af2c1b956b08ff
Izaata <izzataa@code.edu.az>
2023-12-03 21:35:53 +0400

    first

---
+++ src/Loop.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+      /*
+public class Loop {
+    public static void main(String[] args) {
+        /*
+        int counter=0;
+        for (int i = 2; i <= 100; i++) {
+
+            for (int j = 1; j <=i; j++) {
+                if(i%j==0){
+                    counter++;
+                }
+            }
+            if (counter ==2) {
+                System.out.println(i);
+
+            }
+            counter=0;
+
+        }
+
+         */
+      /*  Scanner scanner =new Scanner(System.in);
+
+        System.out.print("Bir ədəd daxil edin: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }*/
+              /*
+        int number=10;
+        Scanner scanner =new Scanner(System.in);
+        while (true) {
+            System.out.print("Bir eded daxil edin: ");
+            int n = scanner.nextInt();
+
+            if (n == number) {
+                System.out.println("Deyeri düz tapdız ");
+               break;
+
+            } else {
+                System.out.println("Deyer " + (n > number ? "yuxarıdı" : "aşağıdı"));
+            }
+        }
+        scanner.close();
+
+               */
+   /*
+
+
+     /*
+        int sumOfEvenNumbers = 0;
+        int sumOfOddNumbers = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+
+                sumOfEvenNumbers += i;
+            } else {
+
+                sumOfOddNumbers += i;
+            }
+        }
+
+        System.out.println("Sum: " + sumOfEvenNumbers);
+        System.out.println("Odd: " + sumOfOddNumbers);
+
+      */
+             /*
+        int count = 10;
+        fibonacciSeries(count);
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void fibonacciSeries(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
+
+
+              */
+
+
+
+
